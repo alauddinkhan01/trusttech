bb505ec294cfdd0e355e5c3f6ed1b4bca2bac119
Alauddin Khan <alauddinkhankpk@gmail.com>
2024-06-02 10:53:22 +0500

    First commit

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -22,49 +22,10 @@ class UserController extends Controller
     public function usersList(Request $request){
        
         return Inertia::render('Admin/Users/UserList');
-        // $request['request_type'] = 'adminDashboard';
-        // $userDetailsList = $this->userService->userDetailsList($request);
-        // $search = $request->search;
-        // return view('admin_dashboard.ContactList',compact('userDetailsList','search'));
     }
 
     public function getAllUsers(){
         return $allUsers = $this->userService->getAllUsers();
     }
-    // // get admin list
-    // public function addContact(Request $request){
-
-    //     // get cities
-    //     $request['model'] ='City';
-    //     $cities = $this->userService->getConfiguration($request);
-        
-    //     // get countries
-    //     $request['model'] ='Country';
-    //     $countries = $this->userService->getConfiguration($request);
-
-    //     // get skills
-    //     $request['model'] ='Skill';
-    //     $skills = $this->userService->getConfiguration($request);
-
-    //     return view('admin_dashboard.AddContact',compact('cities','countries','skills'));
-    // }
-
-    // // store user data
-    // public function storeUserInfo(SubmitUserInfoRequest $request){
-
-    //     $request['user_id'] = Auth::user()->id;
-
-    //     if ($request->skillIds) {
-    //         if (count($request->skillIds) > 0) {
-    //             $request['skillIds'] = array_map('intval',  $request->skillIds) ;
-    //         }
-    //     }
-
-    //     return $this->userService->storeUserInfo($request);
-    // }
-
-    // // delete user data
-    // public function deleteUserInfo($id){
-    //    return $this->userService->deleteUserInfo($id);
-    // }
+    
 }
